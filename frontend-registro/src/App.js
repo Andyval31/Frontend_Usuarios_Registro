ff68b4c77be69d1065e0961d3921dc12db733d41
MatiasFerreiraF <matiferreirafontes@gmail.com>
2025-11-08 16:12:44 -0300

    Front

--- frontend-registro/src/App.js
+++ frontend-registro/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   const [usuarios, setUsuarios] = useState([]);
   const [form, setForm] = useState({ nombre: '', email: '', telefono: '' });
   const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5005";
-
 
   const fetchUsuarios = async () => {
     try {
@@ -28,28 +28,29 @@ function App() {
     }
   };
 
-
   useEffect(() => {
     fetchUsuarios();
   }, []);
 
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Registro de Usuarios</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nombre" value={form.nombre} onChange={e => setForm({ ...form, nombre: e.target.value })} />
-        <input type="email" placeholder="Email" value={form.email} onChange={e => setForm({ ...form, email: e.target.value })} />
-        <input type="text" placeholder="Teléfono" value={form.telefono} onChange={e => setForm({ ...form, telefono: e.target.value })} />
-        <button type="submit">Registrar</button>
-      </form>
+    <BrowserRouter>
+      <div style={{ padding: '20px' }}>
+        <h1>Registro de Usuarios</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Nombre" value={form.nombre} onChange={e => setForm({ ...form, nombre: e.target.value })} />
+          <input type="email" placeholder="Email" value={form.email} onChange={e => setForm({ ...form, email: e.target.value })} />
+          <input type="text" placeholder="Teléfono" value={form.telefono} onChange={e => setForm({ ...form, telefono: e.target.value })} />
+          <button type="submit">Registrar</button>
+        </form>
 
-      <h2>Usuarios Registrados</h2>
-      <ul>
-        {usuarios.map(u => (
-          <li key={u.id}>{u.nombre} - {u.email} - {u.telefono}</li>
-        ))}
-      </ul>
-    </div>
+        <h2>Usuarios Registrados</h2>
+        <ul>
+          {usuarios.map(u => (
+            <li key={u.id}>{u.nombre} - {u.email} - {u.telefono}</li>
+          ))}
+        </ul>
+      </div>
+    </BrowserRouter>
   );
 }
 
